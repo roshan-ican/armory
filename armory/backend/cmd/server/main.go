039aa091dc757a1c438b615de35ea570446c4339
Roshan Ican <roshansahani535@gmail.com>
2026-09-24 17:21:11 +0400

    Add health server and move schema to migrations

---
+++ armory/backend/cmd/server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("ok"))
+	})
+
+	addr := ":8080"
+	log.Printf("Listening on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, mux))
+}
---
+++ armory/backend/go.mod
@@ -0,0 +1,3 @@
+module armory
+
+go 1.26.5
